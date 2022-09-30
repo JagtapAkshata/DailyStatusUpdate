ab39617d0cfa9f3c685f6ac81fafabfebca35e5b
JagtapAkshata <akshata.jagtap@yash.com>
2022-09-30 17:31:37 +0530

    Weather app in react

---
+++ 30Sept22/src/components/Weather.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import DisplayWeather from "./DisplayWeather";
+import "./weather.css";
+
+const Weather = () => {
+  const [weather, setWeather] = useState([]);
+  const [form, setForm] = useState({
+    city: "",
+    country: "",
+  });
+
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name == "city") {
+      setForm({ ...form, city: value });
+    }
+    if (name == "country") {
+      setForm({ ...form, country: value });
+    }
+  };
+  return (
+    <div className="weather">
+      <span className="title">Weather App</span>
+      <br />
+      <form>
+        <input
+          type="text"
+          placeholder="city"
+          name="city"
+          onChange={(e) => handleChange(e)}
+        />
+        &nbsp; &nbsp; &nbsp;&nbsp;
+        <input
+          type="text"
+          placeholder="Country"
+          name="country"
+          onChange={(e) => handleChange(e)}
+        />
+        <button className="getweather" onClick={(e) => weatherData(e)}>
+          Submit
+        </button>
+      </form>
+      {weather.data != undefined ? (
+        <div>
+          <DisplayWeather data={weather.data} />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Weather;
